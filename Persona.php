7329d4dd64142ed3205eb7d7d9acd23a5ecad385
ramonfi3141 <ramon.quintrileo@est.fi.uncoma.edu.ar>
2022-04-14 23:56:49 -0300

    modificacion persona

--- Persona.php
+++ Persona.php
@@ -2,8 +2,7 @@
 
 /**En la clase Persona:
 1. Se registra la siguiente información: nombre, apellido, dni, direccion, mail, telefono y neto.
-2. Método constructor que recibe como parámetros los valores iniciales para los atributos definidos en la
-clase.
+2. Método constructor que recibe como parámetros los valores iniciales para los atributos definidos en la clase.
 3. Los métodos de acceso de cada uno de los atributos de la clase.
 4. Redefinir el método _toString para que retorne la información de los atributos de la clase. */
 
@@ -16,11 +15,8 @@ class Persona{
     private $telefono;
     private $neto;
 
-
-
-    private $numero;
-    
-    function __construct($nombre,$apellido,$dni,$direccion,$mail,$telefono,$neto){
+ 
+    public function __construct($nombre,$apellido,$dni,$direccion,$mail,$telefono,$neto){
         $this->nombre=$nombre;
         $this->apellido=$apellido;
         $this->tipo=$dni;
@@ -28,60 +24,60 @@ class Persona{
         $this->numero=$mail;
         $this->numero=$telefono;
         $this->numero=$neto;
-
     }
     
     
     //setter
-    function setNombre($nombre){
+    public function setNombre($nombre){
         $this->nombre=$nombre;
     }
-    function setApellido($apellido){
+    public function setApellido($apellido){
         $this->apellido=$apellido;
     }
     
-    function setDni($dni){
+    public function setDni($dni){
         $this->dni=$dni;
     }
     
-    function setDire($direccion){
+    public function setDire($direccion){
         $this->direccion=$direccion;
     }
-    function setMail($mail){
+    public function setMail($mail){
         $this->mail=$mail;
 
-    }function setTelefono($telefono){
+    }
+    public function setTelefono($telefono){
         $this->telefono=$telefono;
     }
-    function setNeto($neto){
+    public function setNeto($neto){
         $this->neto=$neto;
     }
     //getter $nombre,$apellido,$dni,$direccion,$mail,$telefono,$neto
-    function getNombre(){
+    public function getNombre(){
         return $this->nombre;
     }
-    function getApellido(){
+    public function getApellido(){
         return $this->apellido;
     }
     
-    function getDni(){
+    public function getDni(){
         return $this->dni;
     }
     
-    function getDire(){
+    public function getDire(){
         return $this->direccion;
     }
     
-    function getMail(){
+    public function getMail(){
         return $this->mail;
     }
-    function getTelefono(){
+    public function getTelefono(){
         return $this->telefono;
     }
-    function getNeto(){
+    public function getNeto(){
         return $this->neto;
     }    
-    function __toString(){
+    public function __toString(){
         return " nombre : " . $this->getNombre()."\n" ." apellido :". $this->getApellido()."\n". " dni : ".$this->getDni()."\n". " direccion : ".$this->getDire() ."\n"." mail :".$this->getMail()."\n"." telefono : ".$this->getTelefono(). "\n"." neto : ". $this->getNeto() . "\n";
     }
     
